fb368d5e34798acc057a0b042d54e000d0d0188d
Julian Porras <juanpo.0812@gmail.com>
2020-09-30 17:31:26 -0500

    Added healthcheck page

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,8 +3,6 @@ const axios = require('axios');
 
 const router = Router();
 
-router.get('/healthcheck', () => res.status(200).send('OK'));
-
 router.get('/', async (_, res) => {
   const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
   return res.status(200).json(response.data);
