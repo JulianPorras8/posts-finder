7d9859a2bcd36452e92d89d9e30781c21d74715a
Julian Porras <juanpo.0812@gmail.com>
2020-09-30 23:07:10 -0500

    Expose correct port on container and config

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,5 @@ COPY . /app
 RUN npm install
 RUN npm run build
 
-EXPOSE 3000
+EXPOSE 80
 CMD pm2-runtime src/server/index.js
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   apiEndpoint: process.env.API_URL || 'http://localhost',
-  port: parseInt(`${process.env.PORT}`, 10) || 3000,
+  port: parseInt(`${process.env.PORT}`, 10) || 80,
 };
 
 module.exports = config;
